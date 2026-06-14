3f84574f10d749ed044f34a300807dbdb232ddf0
Pedro <eupedrobarbosa03@gmail.com>
2026-06-14 15:25:44 -0300

    al

--- dist/js/responsive.js
+++
@@ -1,5 +0,0 @@
-const navbar = document.querySelector("#navbar-container");
-const menu = document.querySelector("#navbar-menu");
-export function responsive() {
-    menu.addEventListener("click", () => navbar.classList.toggle("active"));
-}
